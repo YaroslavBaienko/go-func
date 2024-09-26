e32bdc4a4492e6d3803fcbbbd8667893449ce75f
YaroslavBaienko <zerhug@gmail.com>
2024-09-26 15:04:48 +0300

    go functions basics

--- func-basics/basics.go
+++ func-basics/basics.go
@@ -10,14 +10,23 @@ func main() {
 	numbers := []int{4, 6, 9, 45, 8, 17, 3}
 	learnerResult, learnerError := CalculateMean(numbers)
 	if learnerError != nil {
-		fmt.Printf("ERROR: %v", learnerError)
+		fmt.Printf("Error: %v\n", learnerError)
 	} else {
-		fmt.Println(learnerResult)
+		fmt.Printf("Result: %.6f\n", *learnerResult)
 	}
 }
 
-// CalculateMean returns mean of the numbers slice, or an error.
+// CalculateMean returns the mean of the numbers slice, or an error.
 func CalculateMean(numbers []int) (*float64, error) {
-	// Your code goes here.
-	return nil, errors.New("not implemented")
+	length := len(numbers)
+	if length == 0 {
+		return nil, errors.New("empty numbers")
+	}
+	var sum float64 = 0.0
+	for _, value := range numbers {
+		sum += float64(value)
+	}
+
+	mean := sum / float64(length)
+	return &mean, nil
 }

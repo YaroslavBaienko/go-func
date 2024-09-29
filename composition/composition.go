6c6cec46276cbf21ee3bbf9bb82e18346935eedc
YaroslavBaienko <zerhug@gmail.com>
2024-09-29 21:13:29 +0300

    composition

--- composition/composition.go
+++ composition/composition.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Functions composition in Go")
+}
+
+// CalculateVariance returns variance of the numbers slice, or an error.
+func CalculateVariance(numbers []int) (*float64, error) {
+	return nil, errors.New("not implemented")
 }
